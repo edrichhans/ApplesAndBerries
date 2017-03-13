709c84625a6704fa3101fb02ce5dd5b6aaa5243d
Artemis <Artemis>
2017-03-13 17:01:43 +0800

    updating

---
+++ ApplesAndBerries-master/models/financials/AR.js
@@ -0,0 +1,47 @@
+exports.insert = function(req, res, callBack){
+	var db = req.db;
+	var AR = db.get('AR');
+	var adviceNumbers = db.get('adviceNumbers');
+	var name = req.body.name;
+	var date = req.body.date;
+	var amount = parseFloat(req.body.amount);
+	var particulars = req.body.particulars;
+
+	var issuedBy = req.session.username;
+
+	var currentAdviceNumber;
+
+	adviceNumbers.findOne({"name": "AR"}, function (err, doc) {
+		currentAdviceNumber = doc.number;
+		adviceNumbers.update({"name": "AR"}, {$inc:{"number": 1}});
+		AR.insert({
+			"adviceNumber": currentAdviceNumber,
+			"issuedBy": issuedBy,
+			"name": name,
+			"date": date,
+			"particulars": particulars,
+			"amount": amount
+		});
+	});
+	callBack();
+}
+
+exports.view = function(req, res, callBack){
+	var db = req.db;
+	var AR = db.get('AR');
+
+	AR.find({},function(err, docs){
+		if(err) return callBack(err);
+		callBack(null, docs);
+	});
+}
+
+exports.get = function(req, callBack){
+	var db = req.db;
+	var adviceNumbers = db.get('adviceNumbers');
+
+	adviceNumbers.find({"name": "AR"}, function(err1, an){
+		if(err1) return callBack(err1);
+		callBack(null, an);
+	});
+}
---
+++ ApplesAndBerries-master/robotest/readme.txt
@@ -0,0 +1,3 @@
+Needs chromedriver, python 2.7
+
+https://github.com/robotframework/robotframework/blob/master/INSTALL.rst
