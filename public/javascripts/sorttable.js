0fc03d0a0ab375376ff4fff8218d15860d36d533
bernablues <bernadettemisa403@gmail.com>
2017-03-28 11:54:22 +0800

    added checkboxes

--- public/javascripts/sorttable.js
+++ public/javascripts/sorttable.js
@@ -76,7 +76,7 @@ sorttable = {
 
     // work through each column and calculate its type
     headrow = table.tHead.rows[0].cells;
-    for (var i=0; i<headrow.length; i++) {
+    for (var i=1; i<headrow.length; i++) {
       // manually override the type with a sorttable_type attribute
       if (!headrow[i].className.match(/\bsorttable_nosort\b/)) { // skip this col
         mtch = headrow[i].className.match(/\bsorttable_([a-z0-9]+)\b/);
