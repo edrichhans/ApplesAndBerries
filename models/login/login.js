f15ef17df2570a2b6fe82da4c046be22d62be6ec
Edrich Chua <edrichhans@yahoo.com>
2017-04-18 11:45:06 +0800

    merged from master

--- models/login/login.js
+++ models/login/login.js
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-var crypto = require('crypto');
-
-var genRandomString = function(length){
-	return crypto.randomBytes(Math.ceil(length/2))
-		.toString('hex') /** convert to hexadecimal format */
-		.slice(0,length);	 /** return required number of characters */
-};
-
-var sha512 = function(password, salt){
-	var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-	hash.update(password);
-	var value = hash.digest('hex');
-	return {
-			salt:salt,
-			passwordHash:value
-	};
-};
-
-function isPasswordCorrect(hash, salt, user_password){
-	// console.log('LOG', hash, salt, user_password);
-	return sha512(user_password, salt).passwordHash == hash;
-}
-=======
 var bcrypt = require('bcrypt');
->>>>>>> master
 
 exports.login = function(req, res, callBack) {
 	var db = req.db;
